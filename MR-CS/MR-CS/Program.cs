25d7f7fe5dd1cab2360f038052e821cdf1841c12
TMR <github@kbindustries.us>
2017-05-04 21:51:01 -0400

    Removing "Form1"

--- MR-CS/MR-CS/Program.cs
+++ MR-CS/MR-CS/Program.cs
@@ -16,7 +16,6 @@ namespace MR_CS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
         }
     }
 }
